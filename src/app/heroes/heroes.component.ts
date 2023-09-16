ca8f94c5c280aad4b8dd4e7a7a77242807e955f6
mohammad Nasruddin <nasruddin@nssit.com.my>
2023-09-16 16:30:52 +0800

    learn how to use service how to get data from service must use Observable<>

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Hero } from '../interfaces/hero';
 import { HEROES } from '../mock-heroes';
 import { Component } from '@angular/core';
+import { HeroService } from '../services/hero.service';
+import { MessageService } from '../services/message.service';
 
 
 
@@ -11,14 +13,22 @@ import { Component } from '@angular/core';
 })
 export class HeroesComponent {
 
-  heroes : Hero[] = HEROES;
-
-
+  heroes : Hero[] = [];
   selectedHero?: Hero
+  constructor(private heroService: HeroService, private messageService:MessageService){}
+
+  ngOnInit():void{
+    this.getHeroes();
+  }
+
+  getHeroes():void{
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+    this.messageService.add(`HeroComponent: Selected hero id=${hero.id} name= ${hero.name}`)
   }
-
 
 }
